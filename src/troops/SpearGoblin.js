05d3e2071708622e6c14dcfed72d71a538fc3f22
BuildTools <unconfigured@null.spigotmc.org>
2024-08-07 17:10:17 +0200

    fixes

---
+++ src/troops/SpearGoblin.js
@@ -0,0 +1,14 @@
+class SpearGoblin {
+arrayID=0;
+levels =
+{
+0: [],
+
+9: ["Spear Goblins", "Three unarmored ranged attackers. Who the heck taught these guys to throw spears!?! Who thought that was a good idea?!", 
+    2, [["spear goblin", 110, 110, 67, 0, 0.5, 1, 1, 120, 5, 5.5, 0, 51, 15, -1, false, 0, 0, "ground", "all", 1, 30, 30, []], 
+    ["spear goblin", 110, 110, 67, 0.7, -0.2, 1, 1, 120, 5, 5.5, 0, 51, 15, -1, false, 0, 0, "ground", "all", 1, 36, 30, []], 
+    ["spear goblin", 110, 110, 67, 0.7, 1.2, 1, 1, 120, 5, 5.5, 0, 51, 15, -1, false, 0, 0, "ground", "all", 1, 33, 30, []]]], 
+    
+10: []    
+};
+}
