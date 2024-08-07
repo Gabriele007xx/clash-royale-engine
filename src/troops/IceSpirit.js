43a9691241b31543de04f21bb20247c8ef54d4b2
BuildTools <unconfigured@null.spigotmc.org>
2024-08-07 17:10:17 +0200

    fixes

---
+++ src/troops/IceSpirit.js
@@ -0,0 +1,6 @@
+class IceSpirit
+{
+    levels = {
+    9: ["Ice Spirit", "Spawns one lively little Ice Spirit to freeze a group of enemies. Stay frosty.", 1, [["ice spirit", 190, 190, 91, 0.5, 0.5, 0.8, 1, 120, 2.5, 5.5, 0, 9, 3, -1, false, 0, 1.5, "ground", "all", 1, 30, 30, [], [["kamikaze"], ["stun", 30]]],]], //11
+   } 
+}
